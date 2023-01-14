b06cf9b5d82b7433397d726556f185342f9aa3fb
Combos0 <jcx429.sc2@gmail.com>
2023-01-14 18:32:58 -0500

    getLocalWeather now prints the lat and lon of the city.

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
-async function getLocalWeather(city) {
+async function getLocalWeather() {
     try {
-        let result = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=0a7116bb4c058a3743fcaa50e8b100f6`, {mode: 'cors'});
-        console.log(result);
+        let location = elements.input.value;
+        let result = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=0a7116bb4c058a3743fcaa50e8b100f6`, {mode: 'cors'});
+        let cityName = await result.json();
+        let lat = cityName[0].lat;
+        let lon = cityName[0].lon;
+        console.log(lat, lon);
     } catch (err) {
         console.log(err);
     };
 };
+
+const elements = {
+    input: document.getElementById('city'),
+    button: document.querySelector('button'),
+};
+
+const elementsHandlers = (() => {
+    elements.button.addEventListener('click', getLocalWeather);
+})();

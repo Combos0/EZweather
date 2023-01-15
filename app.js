acfd1a47b89103a6b548efd61e05638f66c36353
Combos0 <jcx429.sc2@gmail.com>
2023-01-14 21:56:45 -0500

    working towards getting  fahrenheit as the default temp scale.

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ let currentInfo = {
     humidity: '',
     windDeg: '',
     windSpeed: '',
+    scale: '',
 };
 
 function storesFetchedData(cityObject) {
@@ -25,6 +26,33 @@ currentInfo.dailyLow = cityObject.main.temp_min;
 currentInfo.humidity = cityObject.main.humidity;
 currentInfo.windDeg = cityObject.wind.deg;
 currentInfo.windSpeed = cityObject.wind.speed;
+currentInfo.scale = 'kelvin';
+};
+
+function kelvinToFahrenheit(K) {
+    let anwser = (1.8 * (K - 273.15) + 32);
+    return anwser;
+};
+
+function fahrenheitToCelcius(F) {
+    let anwser = (((F - 32) * 5) / 9);
+    return anwser;
+};
+
+function celciusToFahrenheit(C) {
+    let anwser = (((C * 9) / 5) + 32);
+    return anwser;
+};
+
+function printsTemps() {
+    console.log(`currentScale: ${currentInfo.scale}, currentTemp: ${currentInfo.currentTemp}, dailyHigh: ${currentInfo.dailyHigh}, dailyLow: ${currentInfo.dailyLow}`)
+}
+
+function getsRidOfKelvin() {
+    currentInfo.currentTemp = kelvinToFahrenheit(currentInfo.currentTemp);
+    currentInfo.dailyHigh = kelvinToFahrenheit(currentInfo.dailyHigh);
+    currentInfo.dailyLow = kelvinToFahrenheit(currentInfo.dailyLow);
+    console.log(currentInfo.currentTemp, currentInfo.dailyHigh, currentInfo.dailyLow);
 };
 
 const elementsHandlers = (() => {

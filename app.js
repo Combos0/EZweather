04e4a894dcb5ec63062568cd8490d1e3aec2b243
Combos0 <jcx429.sc2@gmail.com>
2023-01-14 20:45:53 -0500

    changed getLocalWeather to make correct API call.
    Added holder object for fetched data.

--- app.js
+++ app.js
@@ -1,21 +1,31 @@
-async function getLocalWeather() {
-    try {
-        let location = elements.input.value;
-        let result = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=0a7116bb4c058a3743fcaa50e8b100f6`, {mode: 'cors'});
-        let cityName = await result.json();
-        let lat = cityName[0].lat;
-        let lon = cityName[0].lon;
-        console.log(lat, lon);
-    } catch (err) {
-        console.log(err);
-    };
-};
-
 const elements = {
     input: document.getElementById('city'),
     button: document.querySelector('button'),
 };
 
+let currentInfo = {
+    name: '',
+    lat: '',
+    lon: '',
+    currentTemp: '',
+    dailyHigh: '',
+    dailyLow: '',
+    humidity: '',
+    windDeg: '',
+    windSpeed: '',
+};
+
 const elementsHandlers = (() => {
     elements.button.addEventListener('click', getLocalWeather);
 })();
+
+async function getLocalWeather() {
+    try {
+        let location = elements.input.value;
+        let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=0a7116bb4c058a3743fcaa50e8b100f6`, {mode: 'cors'});
+        let cityInfo = await response.json();
+        console.log(cityInfo);
+    } catch (err) {
+        console.log(err);
+    };
+};

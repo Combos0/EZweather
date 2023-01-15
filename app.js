fb14a3a412dc2e8a0f7b418db6a41bff4a575745
Combos0 <jcx429.sc2@gmail.com>
2023-01-14 20:56:03 -0500

    added function to store searched weather info.

--- app.js
+++ app.js
@@ -15,6 +15,18 @@ let currentInfo = {
     windSpeed: '',
 };
 
+function storesFetchedData(cityObject) {
+currentInfo.name = cityObject.name;
+currentInfo.lat = cityObject.coord.lat;
+currentInfo.lon = cityObject.coord.lon;
+currentInfo.currentTemp = cityObject.main.temp;
+currentInfo.dailyHigh = cityObject.main.temp_max;
+currentInfo.dailyLow = cityObject.main.temp_min;
+currentInfo.humidity = cityObject.main.humidity;
+currentInfo.windDeg = cityObject.wind.deg;
+currentInfo.windSpeed = cityObject.wind.speed;
+};
+
 const elementsHandlers = (() => {
     elements.button.addEventListener('click', getLocalWeather);
 })();
@@ -24,7 +36,8 @@ async function getLocalWeather() {
         let location = elements.input.value;
         let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=0a7116bb4c058a3743fcaa50e8b100f6`, {mode: 'cors'});
         let cityInfo = await response.json();
-        console.log(cityInfo);
+        storesFetchedData(cityInfo);
+        console.log(currentInfo);
     } catch (err) {
         console.log(err);
     };
